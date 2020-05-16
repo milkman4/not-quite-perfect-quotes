26c033ddfa8705c2c031036ad47ff9d2d48fc61b
Matthew Kaufman <matthew.kaufman@ombud.com>
2020-05-16 11:00:02 -0600

    ui api call and state implemented

--- src/App.js
+++ src/App.js
@@ -6,52 +6,60 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      greeting: '',
+      query: '',
+      loading: false,
+      quotes: [],
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    this.setState({ name: event.target.value });
+    this.setState({ query: event.target.value });
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    fetch(`/api/greeting?name=${encodeURIComponent(this.state.name)}`)
+    this.setState({ quotes: [], loading: true });
+    fetch(`/api/quotes?query=${encodeURIComponent(this.state.query)}`)
       .then((response) => response.json())
-      .then((state) => this.setState(state));
+      .then((queryResults) =>
+        this.setState({
+          quotes: queryResults.results,
+          loading: false,
+        })
+      );
   }
 
   render() {
+    const { quotes, loading, query } = this.state;
+    console.log(quotes);
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
           <form onSubmit={this.handleSubmit}>
-            <label htmlFor="name">Enter your name: </label>
+            <label htmlFor="name">Search for a quote...</label>
             <input
               id="name"
               type="text"
-              value={this.state.name}
+              value={query}
               onChange={this.handleChange}
             />
             <button type="submit">Submit</button>
           </form>
-          <p>{this.state.greeting}</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <body>
+          {quotes.length > 0 && !loading
+            ? quotes.map((quote) => {
+                return (
+                  <div key={quote.id}>
+                    {quote.quote}
+                    {quote.attribution}
+                  </div>
+                );
+              })
+            : `No Results`}
+        </body>
       </div>
     );
   }
